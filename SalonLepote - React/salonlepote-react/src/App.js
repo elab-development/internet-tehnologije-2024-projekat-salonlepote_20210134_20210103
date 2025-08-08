dc1fb80e8b9de77fdee0ae0911db179be5515b18
jovana223 <mjovana223@gmail.com>
2025-08-08 12:10:17 +0200

    Dodat css stil

--- SalonLepote - React/salonlepote-react/src/App.js
+++ SalonLepote - React/salonlepote-react/src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import NavBar from './components/NavBar'; 
+import NavBar from './components/NavBar';
+
+ 
 import Home from './pages/Home';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import MakeupArtists from "./pages/makeupArtists";
 import ReservationForm from "./pages/ReservationForm";
 import NotFound from "./pages/NotFound"; // opcionalno
+
+import './styles.css';
+
+
 function App() {
   return (
     <BrowserRouter>
