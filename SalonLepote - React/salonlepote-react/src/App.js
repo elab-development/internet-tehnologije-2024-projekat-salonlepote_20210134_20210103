3f67a7967bbc3980347d68f0aac4b098444fee4d
bojanakostic1 <kosticbojana03@gmail.com>
2025-08-08 11:26:26 +0200

    Premešteni folderi u src i sređene rute u App.js

--- SalonLepote - React/salonlepote-react/src/App.js
+++ SalonLepote - React/salonlepote-react/src/App.js
@@ -4,16 +4,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import NavBar from './components/NavBar'; 
 import Home from './pages/Home';
-
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MakeupArtists from "./pages/makeupArtists";
+import ReservationForm from "./pages/ReservationForm";
+import NotFound from "./pages/NotFound"; // opcionalno
 function App() {
   return (
     <BrowserRouter>
       <div>
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/services" element={<Services />} />
-          <Route path="/reservations" element={<Reservations />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/makeup-artists" element={<MakeupArtists />} />
+        <Route path="/rezervisi" element={<ReservationForm />} />
+        <Route path="*" element={<NotFound />} /> {/* opcionalno */}
         </Routes>
       </div>
     </BrowserRouter>
