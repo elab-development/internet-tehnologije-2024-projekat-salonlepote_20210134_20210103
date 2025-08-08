e53413ef546135a21071476eea796c224e54ceac
bojanakostic1 <kosticbojana03@gmail.com>
2025-08-08 15:34:27 +0200

    Dodat css za NavBar

--- SalonLepote - React/salonlepote-react/src/components/NavBar.jsx
+++ SalonLepote - React/salonlepote-react/src/components/NavBar.jsx
@@ -1,15 +1,26 @@
 import { Link } from "react-router-dom";
+import "../styles/NavBar.css";
+import logoImage from "../assets/logo-bez-pozadine.png";
 
 function NavBar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Početna</Link></li>
-        <li><Link to="/makeup-artists">Šminkeri</Link></li>
-        <li><Link to="/rezervisi">Rezerviši</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Registracija</Link></li>
-      </ul>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo" style={{ display: 'flex', alignItems: 'center' }}>
+          <img
+            src={logoImage}
+            alt="J&B Beauty Lounge Logo"
+            style={{ height: "50px", marginRight: "10px" }}
+          />
+          <Link to="/">J&B Beauty Lounge</Link>
+        </div>
+        <ul className="navbar-menu">
+          <li><Link to="/">Početna</Link></li>
+          <li><Link to="/makeup-artists">Šminkeri</Link></li>
+          <li><Link to="/rezervisi">Rezerviši</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      </div>
     </nav>
   );
 }
